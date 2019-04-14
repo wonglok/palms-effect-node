43b65c9e992c7c406fb68022040dc62f64719795
Wong Lok <yellowhappy831@gmail.com>
2019-04-14 18:14:22 +0800

    github backup

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+let baseCSS = `
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    z-index: 10;
+    height: 100%;
+    background-color: #f1f1f1;
+`
+
+document.querySelector('html').style.overflow = 'hidden'
+
+let base = document.createElement('div')
+base.style.cssText = baseCSS
+document.body.appendChild(base)
+
 new Vue({
   render: h => h(App)
-}).$mount('#yo-enos')
+}).$mount(base)
+
+/*
+
+<script src="https://palms-effect-node.netlify.com/js/chunk-vendors.js"></script>
+<script src="https://palms-effect-node.netlify.com/js/app.js"></script>
+
+*/
